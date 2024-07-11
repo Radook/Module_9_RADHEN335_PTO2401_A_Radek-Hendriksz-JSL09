d20119e93daf7fc11441d4f7f5dae6f81b05fdf0
Radek <radekhendriksz@gmail.com>
2024-07-11 15:29:55 +0200

    wallpaper, alignment and crypto prices completed

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-/**
- * Challenge: get a random image from Unsplash and set it as the background
- * 
- * Part 1:
- * 
- * URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
- * (You can change the "query" at the end to whatever theme you want)
- * 
- * Change the body's backgroundImage to: 
- * `url(<insert the URL of the iamge from the API here>)`
- * 
- * (You may need to dig around the response body a bit to find this URL)
- * 
- * (Note I've already added some CSS to resize the image within the window.
- * Instructions for this were found on CSS Tricks: 
- * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
- */
 
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=3drenders")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.urls.regular)
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+        document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+        `
+
+        document.getElementById("crypto").innerHTML += `
+        <p>🎯: R${data.market_data.current_price.zar}</p>
+        <p>🔼: R${data.market_data.high_24h.zar}</p>
+        <p>🔻: R${data.market_data.low_24h.zar}</p>
+        `
+    })
+    .catch(err => console.error(err))
