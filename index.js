beda5aaf3ea0b759df79a4d4d9d694aa117d4aad
Radek <radekhendriksz@gmail.com>
2024-07-12 13:44:13 +0200

    chuck norris api

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ try {
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
         document.getElementById("author").textContent = `By: ${data.user.name}` 
 }
-    
+
+try {
+    const quoteRes = await fetch("https://api.chucknorris.io/jokes/random");
+        const quoteData = await quoteRes.json();
+        document.getElementById("humour").innerHTML = quoteData.value;
+    } catch (err) {
+        document.getElementById("humour").innerHTML = "Oops! Couldn't fetch a funny quote. Here's a default one: 'Why don’t scientists trust atoms? Because they make up everything!'";
+    }
 
 try {
 const res = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
